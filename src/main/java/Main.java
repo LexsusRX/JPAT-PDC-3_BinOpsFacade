9272970cdd31b9979f7ade9e71a068636d497120
LexsusRX <LexsusRX@yandex.ru>
2022-05-06 21:34:11 +0400

    готово к сдаче

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+
+        System.out.println(binOps.sum("1000111101", "110000110101"));
+        System.out.println(binOps.mult("111101110", "10000111"));
+    }
+}
